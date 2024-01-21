dfc9323a65a2d9473da1d27791b00e9b1d39a334
Julie <julpraza@gmail.com>
2024-01-21 13:45:16 +0000

    index.html returned

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "manifest.json": "f1a76fcb8d0ea9acf20fc853ad3c369a",
-"index.html": "84dbe71d2bf749661bbaac84555ed37f",
-"/": "84dbe71d2bf749661bbaac84555ed37f",
+"index.html": "f3bb41639489e2469b05ef3784e6032a",
+"/": "f3bb41639489e2469b05ef3784e6032a",
 "assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9",
 "assets/fonts/MaterialIcons-Regular.otf": "75c0644b677100cc197b81ee4f2431f6",
 "assets/AssetManifest.bin.json": "a1fee2517bf598633e2f67fcf3e26c94",
